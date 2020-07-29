5b44f191d686edbee8989cbc6a1b54304a7b08d8
Raúl M <between13131@gmail.com>
2020-07-29 04:55:40 +0200

    Created validateURL method
    This will validate URLs using Java's built-in checker

--- src/main/java/me/bettd/accuratereadings/Methods.java
+++ src/main/java/me/bettd/accuratereadings/Methods.java
@@ -2,8 +2,23 @@ package me.bettd.accuratereadings;
 
 import org.bukkit.ChatColor;
 
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
 public class Methods {
     public static String convert(String input) {
         return ChatColor.translateAlternateColorCodes('&', input);
+    }
+
+    public static boolean validateURL(String url)
+    {
+        try {
+            new URL(url).toURI();
+            return true;
+        }
+        catch (URISyntaxException | MalformedURLException e) {
+            return false;
+        }
     }
 }
