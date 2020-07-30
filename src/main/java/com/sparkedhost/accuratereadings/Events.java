4a14fe82798d38afcfb819fb0e1204fea66496c1
Raúl M <between13131@gmail.com>
2020-07-30 06:30:39 +0200

    Actually fixed the /restart issue

--- src/main/java/com/sparkedhost/accuratereadings/Events.java
+++ src/main/java/com/sparkedhost/accuratereadings/Events.java
@@ -5,13 +5,15 @@ import com.stanjg.ptero4j.entities.objects.server.PowerAction;
 import com.stanjg.ptero4j.entities.panel.user.UserServer;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
 public class Events implements Listener {
-    @EventHandler
-    public void onRestartCommand(AsyncPlayerChatEvent e) {
-        if(e.getMessage().startsWith("/restart")) {
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onRestartCommand(PlayerCommandPreprocessEvent e) {
+        if(e.getMessage().startsWith("/restart") || e.getMessage().equals("/restart")) {
             PteroUserAPI api = new PteroUserAPI(Bukkit.getPluginManager().getPlugin("AccurateReadings").getConfig().getString("panelUrl"), Bukkit.getPluginManager().getPlugin("AccurateReadings").getConfig().getString("apiKey"));
             UserServer server = api.getServersController().getServer(Bukkit.getPluginManager().getPlugin("AccurateReadings").getConfig().getString("serverId"));
             server.restart();
