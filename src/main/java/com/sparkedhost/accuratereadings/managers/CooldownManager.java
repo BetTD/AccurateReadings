fe126e99f1bac36103a8672997c9f343d055dd2f
Raúl M <between13131@gmail.com>
2022-03-16 01:13:43 +0100

    Use ConcurrentHashMap instead of HashMap

--- src/main/java/com/sparkedhost/accuratereadings/managers/CooldownManager.java
+++ src/main/java/com/sparkedhost/accuratereadings/managers/CooldownManager.java
@@ -4,13 +4,19 @@ package com.sparkedhost.accuratereadings.managers;
 
 import com.sparkedhost.accuratereadings.Main;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+/*
+    This is very legacy code... I don't see a real legitimate use for a cooldown feature anymore, since the resource
+    usage checks have been moved to an async routine. Cooldown is disabled by default for that very reason, but I'll
+    keep this code here just in case someone wants to use the feature.
+ */
 
 public class CooldownManager {
 
-    private final Map<UUID, Long> cooldowns = new HashMap<>();
+    private final Map<UUID, Long> cooldowns = new ConcurrentHashMap<>();
 
     public static final int DEFAULT_COOLDOWN = Main.getInstance().getSettings().cooldown_time;
 
