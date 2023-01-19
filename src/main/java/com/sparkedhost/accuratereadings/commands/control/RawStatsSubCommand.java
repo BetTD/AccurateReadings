0e2088fe0cb05021af62de99f1a2416578550831
Raúl M <raul.m@sparkedhost.com>
2023-01-19 20:48:44 +0100

    Add basic environment information to raw subcommand

--- src/main/java/com/sparkedhost/accuratereadings/commands/control/RawStatsSubCommand.java
+++ src/main/java/com/sparkedhost/accuratereadings/commands/control/RawStatsSubCommand.java
@@ -5,6 +5,7 @@ import com.sparkedhost.accuratereadings.commands.BaseCommand;
 import com.sparkedhost.accuratereadings.commands.SubCommand;
 import com.sparkedhost.accuratereadings.config.Settings;
 import com.sparkedhost.accuratereadings.managers.PterodactylManager;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
@@ -13,6 +14,7 @@ import java.util.StringJoiner;
 public class RawStatsSubCommand extends SubCommand {
     Settings settings = Main.getInstance().getSettings();
     PterodactylManager pterodactylManager = Main.getInstance().getPteroAPI();
+
     protected RawStatsSubCommand(BaseCommand baseCommand) {
         super(baseCommand, null, "Debugging command, shows raw stats values.");
     }
@@ -20,7 +22,24 @@ public class RawStatsSubCommand extends SubCommand {
     public void execute(CommandSender sender, Command command, String[] args) {
         StringJoiner output = new StringJoiner("\n");
 
-        output.add(settings.pterodactyl_useWebsocket ? "Using websocket connection" : "Using standard API polling");
+        output.add("---- ENVIRONMENT INFO ----");
+
+        output.add("Server version: " + Bukkit.getVersion());
+        output.add("Java VM name: " + System.getProperty("java.vm.name"));
+        output.add("Java VM vendor: " + System.getProperty("java.vm.vendor"));
+        output.add("Java version: " + System.getProperty("java.runtime.version"));
+        output.add(
+                Main.getInstance().getName() +
+                " version: " +
+                Main.getInstance().getDescription().getVersion()
+        );
+
+        output.add("");
+        output.add("---- PTERODACTYL INFO ----");
+
+        output.add(settings.pterodactyl_useWebsocket
+                ? "Using websocket connection"
+                : "Using standard API polling");
         output.add("Server ID: " + pterodactylManager.getServerId());
         output.add("Is server owner: " + (pterodactylManager.isServerOwner() ? "yes" : "no"));
 
